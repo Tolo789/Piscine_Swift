f684f376d9c5129e089dc3d3f50bbc2eccb2f4a5
Claudio MUTTI <cmutti@e2r7p2.42.fr>
2018-10-02 12:37:25 +0200

    d01 ex02

---
+++ d01/ex02/Deck.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Deck : NSObject {
+    static let allSpades = generateAllCardsOf(type: Color.Spade)
+    static let allDiamonds = generateAllCardsOf(type: Color.Diamond)
+    static let allHearts = generateAllCardsOf(type: Color.Heart)
+    static let allClubs = generateAllCardsOf(type: Color.Club)
+    static let allCards = allSpades + allDiamonds + allHearts + allClubs
+
+    static func generateAllCardsOf(type: Color) -> [Card] {
+        var tmpList = [Card]()
+        for value in Value.allValues {
+            tmpList.append(Card(c: type, v: value))
+        }
+
+        return tmpList
+    }
+}
---
+++ d01/ex02/main.swift
@@ -0,0 +1,20 @@
+print("--- All Spades ------------------------")
+for card in Deck.allSpades {
+    print (card)
+}
+print("--- All Diamonds ------------------------")
+for card in Deck.allDiamonds {
+    print (card)
+}
+print("--- All Hearts ------------------------")
+for card in Deck.allHearts {
+    print (card)
+}
+print("--- All Clubs ------------------------")
+for card in Deck.allClubs {
+    print (card)
+}
+print("--- All Cards ------------------------")
+for card in Deck.allCards {
+    print (card)
+}
